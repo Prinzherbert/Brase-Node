d8d523be039e7d0b7bd8c4e209d28665b200a644
Prinzherbert <pikachu.dat40@gmail.com>
2021-12-05 15:58:01 -0300

    Teste

--- Java.js
+++ Java.js
@@ -64,8 +64,12 @@ socket.onmessage = ({data}) => { // Quando receber uma mensagem do servidor
     postItArray[info[1]].text = info[2];
     break;
   case "move":
-    postItArray[info[1]].x = info[2];
-    postItArray[info[1]].y = info[3];
+    setTimeout(function(){
+      if (selectedPostIt = null){
+        postItArray[info[1]].x = info[2];
+        postItArray[info[1]].y = info[3];
+      }
+    }, 100);
     break;
   case "array":
     postItArray = [];
