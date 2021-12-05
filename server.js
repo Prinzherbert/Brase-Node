99166a706e986871ac59f024a4dbd7d23dffb308
Prinzherbert <pikachu.dat40@gmail.com>
2021-12-05 12:53:50 -0300

    Teste

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
-//.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const server = express().listen(PORT).use((req, res) => res.sendFile(INDEX, { root: __dirname }));
 //
+//.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
 
